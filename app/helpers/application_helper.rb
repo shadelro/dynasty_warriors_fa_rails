b3e81c68da71fc2779f48b2f5dd5531d9f99a5aa
shadelro <rbshadel@gmail.com>
2014-07-10 17:11:07 -0500

    cache current_team

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def current_team
-    Team.find_by(league_id: current_league.id, user_id: current_user.id)
+    @current_team ||= current_user && current_league && Team.find_by(league_id: current_league.id, user_id: current_user.id)
   end
 
   def current_league_name
