5c0cce124ea0d0d505b4428054e8540f0fa6b257
shadelro <rbshadel@gmail.com>
2014-07-10 16:33:52 -0500

    display top bid amounts

---
+++ app/helpers/players_helper.rb
@@ -0,0 +1,10 @@
+module PlayersHelper
+  def top_bidder(player, league_id)
+    top_bidder = player.top_bidder(league_id)
+    top_bid = player.top_bid(league_id)
+
+    if top_bidder.present? && top_bid.present?
+      "#{top_bidder.name}: #{number_to_currency(top_bid.amount)}"
+    end
+  end
+end
