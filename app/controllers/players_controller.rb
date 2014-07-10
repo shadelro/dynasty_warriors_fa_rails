d12e1983c503dcdd571606ed858cc176b36fefd5
shadelro <rbshadel@gmail.com>
2014-07-09 23:59:06 -0500

    fix bid team

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,8 @@ class PlayersController < ApplicationController
 
   def show
     @player = Player.find_by(id: player_params[:id])
-    @my_bid = Bid.find_by(player_id: player_params[:id], team: current_user.team_for_league(player_params[:league_id]))
+    @my_team = current_user.team_for_league(player_params[:league_id])
+    @my_bid = Bid.find_by(player_id: player_params[:id], team: @my_team)
   end
 
   private
