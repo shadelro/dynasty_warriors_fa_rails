f56e4965541f6038d9647c8bb66448fd1830f3b4
shadelro <rbshadel@gmail.com>
2014-07-09 23:46:19 -0500

    fix please god

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,5 @@
 class Invitation < ActiveRecord::Base
   belongs_to :league
   belongs_to :user
-  belongs_to :owner, class_name: :User, foreign_key: :owner_id
+  belongs_to :owner, class_name: :User
 end
--- db/migrate/20140705030558_create_invitations.rb
+++ db/migrate/20140705030558_create_invitations.rb
@@ -2,7 +2,7 @@ class CreateInvitations < ActiveRecord::Migration
   def change
     create_table :invitations do |t|
       t.integer :owner_id
-      t.string :user_id
+      t.integer :user_id
       t.integer :league_id
 
       t.timestamps
