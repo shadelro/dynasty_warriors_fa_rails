f9fd900d8aa16747a20500f59ad1e573ceaeab2b
shadelro <rbshadel@gmail.com>
2014-07-11 17:55:38 -0500

    add link for all leagues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
     (current_league && current_league.name) || 'Select a league'
   end
 
+  def current_league_path
+    (current_league && league_path(current_league)) || '#'
+  end
+
   def current_team_display
     "#{current_team.name} (#{number_to_currency(current_team.remaining_salary, precision: 0)})"
   end
